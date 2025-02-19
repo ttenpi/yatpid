e0de1085b90b9a3707b829e54956bd744ba987fd
Daphne de Waal <d@waal.me>
2025-02-19 17:16:20 +0100

    feat: enhance footer layout with navigation links and licensing information

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>
-    footer
-  </div>
+  <footer>
+    <hr>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="https://tenpi.li/mi">tonsi tenpi
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">about
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="https://github.com/ttenpi/yatpid">
+            GitHub</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="https://github.com/ttenpi/yatpid/blob/main/assets/data/dataset.toml">
+            Dataset</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <p>code licensed under <NuxtLink
+        to="https://github.com/ttenpi/yatpid/blob/main/LICENSE.md">
+        OQL</NuxtLink>
+    </p>
+    <p>dataset licensed under <NuxtLink to="">CC0</NuxtLink></p>
+  </footer>
 </template>
+
+<style>
+  footer > nav ul {
+    display: flex;
+    list-style: none;
+    gap: 1rem;
+    margin: 0;
+    padding: 0;
+  }
+</style>
