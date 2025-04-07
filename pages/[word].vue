c95003f467cbef9d7fcdfbedce447b65f86525ce
Daphne de Waal <d@waal.me>
2025-04-07 21:15:53 +0200

    add example section to word display and enhance semantic category labeling

--- pages/[word].vue
+++ pages/[word].vue
@@ -1,48 +1,89 @@
 <template>
-  <p><NuxtLink to="/">back to dictionary</NuxtLink></p>
+  <p>
+    <NuxtLink to="/">back to dictionary</NuxtLink>
+  </p>
   <hr />
-  <h1>{{ wordData ? wordData.lemma : route.params.word }}</h1>
+
+  <h1>
+    {{ wordData ? wordData.lemma : route.params.word }}
+  </h1>
+
+  <!-- Loading -->
   <p v-if="status === 'pending'">Loading...</p>
   <div v-if="error">
     <p><strong>Word not found</strong></p>
     <p>
-      Error: <code>{{ error }}</code>
+      <code>{{ error }}</code>
     </p>
   </div>
+
   <div v-else-if="wordData">
     <dl>
-      <dt v-if="wordData">
+      <!-- Lemma -->
+      <dt>
         {{ wordData.lemma }}
-        <em v-if="wordData.subtitle">({{ wordData.subtitle }})</em>
       </dt>
+      <!-- Subtitle -->
+      <small v-if="wordData.subtitle">
+        ({{ wordData.subtitle }})
+      </small>
+      <!-- Data -->
       <dd v-if="wordData && wordData.semanticCategory">
-        ({{ wordData.semanticCategory }})
+        (Categories: {{ wordData.semanticCategory }})
       </dd>
-      <dd v-if="wordData && wordData.definitions && wordData.definitions.en">
+      <!-- Definition EN -->
+      <dd
+        v-if="wordData && wordData.definitions && wordData.definitions.en">
         (EN) {{ wordData.definitions.en }}
       </dd>
-      <dd v-if="wordData && wordData.definitions && wordData.definitions.nl">
+      <!-- Definition NL -->
+      <dd
+        v-if="wordData && wordData.definitions && wordData.definitions.nl">
         (NL) {{ wordData.definitions.nl }}
       </dd>
     </dl>
+
+    <!-- Examples -->
+    <section class="examples"
+      v-if="wordData && wordData.example">
+      <h2>Examples:</h2>
+      <dl v-for="(example, index) in wordData.example"
+        :key="index">
+        <dt>
+          {{ example.tok }}
+        </dt>
+        <small v-if="example.subtitle">
+          ({{ example.subtitle }})
+        </small>
+        <dd v-if="example.en">(EN) {{ example.en }}</dd>
+        <dd v-if="example.nl">(NL) {{ example.nl }}</dd>
+      </dl>
+    </section>
+
   </div>
 </template>
 
 <script setup lang="ts">
-interface WordData {
-  lemma?: string;
-  subtitle?: string;
-  semanticCategory?: Array<string>;
-  definitions?: {
-    en?: string;
-    nl?: string;
-  };
-}
+  interface WordData {
+    lemma?: string
+    subtitle?: string
+    semanticCategory?: Array<string>
+    definitions?: {
+      en?: string
+      nl?: string
+    }
+    example?: Array<{
+      subtitle?: string
+      tok: string
+      en?: string
+      nl?: string
+    }>
+  }
 
-const route = useRoute();
-const {
-  data: wordData,
-  status,
-  error,
-} = useFetch<WordData>(`/api/dictionary?word=${route.params.word}`);
+  const route = useRoute()
+  const {
+    data: wordData,
+    status,
+    error,
+  } = useFetch<WordData>(`/api/dictionary?word=${route.params.word}`)
 </script>
