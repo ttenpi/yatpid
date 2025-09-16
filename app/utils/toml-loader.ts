7f7edbfd6eb6c71d5ace030437881cc2b2d636da
Daphne de Waal <d@waal.me>
2025-09-16 23:20:06 +0200

    new directory structure

--- app/utils/toml-loader.ts
+++ app/utils/toml-loader.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import toml from '@iarna/toml'
 
 export default function tomlLoader(fileName: string): any {
-  const filePath = path.resolve('assets/data', fileName)
+  const filePath = path.resolve('app/assets/data', fileName)
   const fileContent = fs.readFileSync(filePath, 'utf-8')
   return toml.parse(fileContent)
 }
