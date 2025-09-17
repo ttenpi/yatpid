adf84ff06e4ead9d3e5c1476d193d662920b0713
Daphne de Waal <d@waal.me>
2025-09-17 02:46:56 +0200

    feat(dict): :sparkles: add test page with multiple dictionary entries
    * Introduced a new test page to showcase various dictionary entries.
    * This enhances the user experience by providing a comprehensive view of available terms.

---
+++ app/pages/test.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <DictGroupContainer>
+      <DictGroup title="testing 1">
+        <DictEntry term="wawa" />
+        <DictEntry term="tenpi" />
+        <DictEntry term="akesi" />
+      </DictGroup>
+      <DictGroup title="testing 1.5">
+        <DictEntry term="telo" />
+        <DictEntry term="kule" />
+        <DictEntry term="suno" />
+      </DictGroup>
+      <DictGroup title="testing 2">
+        <DictEntry term="kijetesantakalu" />
+        <DictEntry term="akesi" />
+        <DictEntry term="ala" />
+        <DictEntry term="melisma" />
+      </DictGroup>
+      <DictGroup title="testing 2.5">
+        <DictEntry term="pakala" />
+        <DictEntry term="kepeken" />
+        <DictEntry term="sama" />
+      </DictGroup>
+      <DictGroup title="testing 3">
+        <DictEntry term="jan" />
+        <DictEntry term="monumentalise" />
+      </DictGroup>
+      <DictGroup title="testing 4">
+        <DictEntry term="ala" />
+        <DictEntry term="anpa" />
+      </DictGroup>
+      <DictGroup title="testing 5">
+        <DictEntry term="tonsi" />
+      </DictGroup>
+    </DictGroupContainer>
+  </div>
+</template>
